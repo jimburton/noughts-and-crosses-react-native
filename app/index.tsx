fcb7f44a202d20483e511911fd123e24ef458e40
Jim Burton <jimburton1@gmail.com>
2024-12-01 23:02:09 +0000

    Fixed display of winner.

--- app/index.tsx
+++ app/index.tsx
@@ -1,21 +1,26 @@
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
-import './App.css';
 import Row from './Row';
 
 export default function Index() {
     const [xIsNext, setXIsNext] = useState(true);
     const [gameOver, setGameOver] = useState(false);
     const [squares, setSquares] = useState(Array(9).fill(" "));
+    const [status, setStatus] = useState("");
     const winner = calculateWinner(squares);
-    let status = "";
-    setStatus(winner);
+    setStatusStr(winner);
 
-    function setStatus(winner) {
-        if (winner) {
-            status = "Winner: " + winner;
-        } else {
-            status = "Next player: " + (xIsNext ? "X" : "O");
+    function setStatusStr(winner) {
+        if (!gameOver) {
+            let statusStr;
+            if (winner) {
+                statusStr = "Winner: " + winner;
+            } else {
+                statusStr = "Next player: " + (xIsNext ? "X" : "O");
+            }
+            if(statusStr != status) {
+                setStatus(statusStr);
+            }
         }
     }
 
@@ -24,19 +29,6 @@ export default function Index() {
         setXIsNext(true);
         setGameOver(false);
     }
-
-    /*useEffect(() => {
-        if (gameOver) {
-            console.log(`initializing interval`);
-            const interval = setInterval(() => {
-                reset();
-            }, 1000);
-            return () => {
-                console.log("clearing interval");
-                clearInterval(interval);
-            }
-        }
-    }, [gameOver,setGameOver]);*/
 
     function calculateWinner(squares) {
         if (!gameOver) {
@@ -59,8 +51,7 @@ export default function Index() {
                 }
             }
             if(winner) {
-                console.log("we have a winner");
-                setStatus(winner);
+                setStatusStr(winner);
                 setGameOver(true);
             }
             return winner;
@@ -69,8 +60,7 @@ export default function Index() {
 
     function handleClick(i: number) {
         if (!gameOver) {
-            if (squares[i] != " " || calculateWinner(squares)) {
-                console.log("square is occupied or game is over");
+            if (squares[i] != " ") {
                 return;
             }
 
@@ -81,8 +71,8 @@ export default function Index() {
                 nextSquares[i] = "O";
             }
             setSquares(nextSquares);
-        
-            if (!calculateWinner(squares)) {
+            const winner = calculateWinner(squares);
+            if (!winner) {
                 setXIsNext(!xIsNext);
             }
         }
@@ -94,7 +84,7 @@ export default function Index() {
         {[0,1,2].map((m) => { return <Row key={m} rowNum={m} squares={squares} handleClick={handleClick}/> })}
         <TouchableOpacity style={[styles.resetButton,
                         {opacity: gameOver ? 100 : 0}]}
-                    onPress={reset}><Text>Reset</Text></TouchableOpacity>
+                    onPress={reset}><Text style={styles.resetButtonText}>Reset</Text></TouchableOpacity>
         </>
     )
 }
@@ -102,13 +92,24 @@ const styles = StyleSheet.create({
     resetButton: {
         alignItems: 'center',
         justifyContent: 'center',
+        marginTop: 10,
         paddingVertical: 40,
         paddingHorizontal: 40,
         borderWidth: 2,
         borderColor: 'black',
         elevation: 3,
-        
-        backgroundColor: 'darkslategray',
+        backgroundColor: 'white',
+    },
+    resetButtonText: {
+        fontSize: 17,
+        fontFamily: 'monospace',
+        lineHeight: 21,
+        fontWeight: 'bold',
+        letterSpacing: 0.25,
+        color: 'black',
+        padding: 10,
+        justifyContent: 'center',
+        alignItems: 'center',
     },
     statusContainer: {
         flex: 1,
